e8a6ded10ec662bfe5cd382707e21943fd339312
Akshay Sharma <akshay@kiot.io>
2020-04-02 17:48:03 +0530

    [Add] pm2 init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,31 @@
 module.exports = {
-    apps: [{
-        "name": "Document Sharer",
-        "instances": 1,
-        "script": "./bin/www",
-        "watch": false,
-        "ignore_watch": [
-            "node_modules",
-            "logs",
-            ".git"
-        ],
-        "error_file": "./logs/errors.log",
-        "out_file": "./logs/errors_out.log",
-        "log_date_format": "YYYY-MM-DD HH:mm Z",
-        env: {
-            "NODE_ENV": "development",
-        },
-        env_production: {
-            "NODE_ENV": "production",
-            "ENVIRONMENT": "production",
-            "PORT": "8002"
-        }
-    }]
-}
+  apps : [{
+    name: 'API',
+    script: './bin/www',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+        "NODE_ENV": "production",
+        "ENVIRONMENT": "production"
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
