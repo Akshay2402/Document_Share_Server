667a80f9b1757de26fd207492aa5b4d45d8374d6
Akshay Sharma <akshay@kiot.io>
2020-04-02 16:04:24 +0530

    [Add] ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    apps: [{
+        "name": "Document Sharer",
+        "script": "./bin/www",
+        "watch": false,
+        "ignore_watch": [
+            "node_modules",
+            "logs",
+            ".git"
+        ],
+        "error_file": "./logs/errors.log",
+        "out_file": "./logs/errors_out.log",
+        "log_date_format": "YYYY-MM-DD HH:mm Z",
+        env: {
+            "NODE_ENV": "development",
+        },
+        env_production: {
+            "NODE_ENV": "production",
+            "ENVIRONMENT": "production",
+            "PORT": "8002"
+        }
+    }]
+}
